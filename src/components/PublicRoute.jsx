5ca89b16ce7f4bfe1aed3b4ce3bcc0fa6c494cce
kayes360 <imrulkayes560@gmail.com>
2023-01-03 00:43:28 +0600

    L-10

---
+++ src/components/PublicRoute.jsx
@@ -0,0 +1,22 @@
+// import {useAuth} from '../contexts/AuthContext'
+// import {BrowserRouter as Route,useNavigate ,  Navigate, Outlet } from 'react-router-dom' 
+
+// export default function PrivateRoute({element: Element, ...rest}) {
+//     const {currentUser} = useAuth();
+//     const navigate = useNavigate();
+     
+//     // return currentUser ? <Navigate to="/quiz" />: <Navigate to="/login" />;
+//     return currentUser ? (
+//         <Route {...rest}> {(props) => <Element {...props}/> }</Route>
+//     ): (
+//         <Navigate to="/login" />
+//     ) ;
+//     ; 
+// } 
+
+import { Outlet, Navigate  } from "react-router-dom"; 
+import {useAuth} from '../contexts/AuthContext'
+export default function PublicRoute() {
+  const { currentUser } = useAuth(); 
+  return !currentUser ? <Outlet/>: <Navigate to='/' />;
+}
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function Video() {
   return (
-    <div>
-      <a href="quiz.html">
+    <div> 
         <div className={classes.video}>
           <img src={image} alt="" />
           <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
@@ -13,8 +12,7 @@ export default function Video() {
             <p>10 Questions</p>
             <p>Score : Not taken yet</p>
           </div>
-        </div>
-      </a>
+        </div> 
     </div>
   );
 }
