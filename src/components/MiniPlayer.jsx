639f23c054aa6fe1782d470bd6a83c7fcb40132a
kayes360 <imrulkayes560@gmail.com>
2023-01-10 01:50:16 +0600

    L-15

--- src/components/MiniPlayer.jsx
+++ src/components/MiniPlayer.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "../styles/MiniPlayer.module.css";
 import MiniPlayerImage from "../assets/images/3.jpg";
+import ReactPlayer from "react-player/youtube";
 
+export default function MiniPlayer({ id, title }) {
+  const buttonRef = useRef();
+  const [status, setStatus] = useState(false);
+  const videoURL = `https://www.youtube.com/watch?v=${id}`;
 
-export default function MiniPlayer() {
+  function toggleMiniPlayer() {
+    if (!status) {
+      buttonRef.current.classList.remove(classes.floatingBtn);
+      setStatus(true);
+    } else {
+      buttonRef.current.classList.add(classes.floatingBtn);
+      setStatus(false);
+    }
+  }
   return (
-    <div className={`${classes.miniPlayer} ${classes.floatingBtn}`}>
-      <span className={`material-icons-outlined ${classes.open}`}> play_circle_filled </span>
-      <span className={`material-icons-outlined ${classes.close}`}> close </span>
-      <img src={MiniPlayerImage} alt="" />
-      <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+    <div
+      className={`${classes.miniPlayer} ${classes.floatingBtn}`}
+      ref={buttonRef}
+      onClick={toggleMiniPlayer}
+    >
+      <span className={`material-icons-outlined ${classes.open}`}>
+        {" "}
+        play_circle_filled{" "}
+      </span>
+      <span
+        className={`material-icons-outlined ${classes.close}`}
+        onClick={toggleMiniPlayer}
+      >
+        {" "}
+        close{" "}
+      </span>  
+      <ReactPlayer 
+      className={classes.player}
+        url={videoURL} 
+      />
+      <p>{title}</p>
     </div>
   );
 }
